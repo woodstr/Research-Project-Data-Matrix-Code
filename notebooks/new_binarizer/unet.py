e2b06ddc26630a97ecb2da5d8acc502dd3240877
Aidan Stocks <aist@itu.dk>
2024-12-15 23:13:51 +0100

    final edits

--- notebooks/new_binarizer/unet.py
+++ notebooks/new_binarizer/unet.py
@@ -8,30 +8,30 @@ class binarizer(nn.Module):
         super(binarizer, self).__init__()
         
         # Encoder
-        self.enc1 = self.double_conv(in_channels, 64)
-        self.enc2 = self.double_conv(64, 128)
-        self.enc3 = self.double_conv(128, 256)
-        self.enc4 = self.double_conv(256, 512)
+        self.enc1 = self.double_conv(in_channels, 64) # with input 512x512x1, output 512x512x64
+        self.enc2 = self.double_conv(64, 128) # with input 512x512x64, output 256x256x128
+        self.enc3 = self.double_conv(128, 256) # with input 256x256x128, output 128x128x256
+        self.enc4 = self.double_conv(256, 512) # with input 128x128x256, output 64x64x512
         
         # Bottleneck
-        self.bottleneck = self.double_conv(512, 1024)
+        self.bottleneck = self.double_conv(512, 1024) # with input 64x64x512, output 32x32x1024
         
         # Decoder
-        self.dec4 = self.upconv(1024, 512)
-        self.up4 = self.double_conv(1024, 512)
+        self.dec4 = self.upconv(1024, 512) # with input 32x32x1024, output 64x64x512
+        self.up4 = self.double_conv(1024, 512) # with input 64x64x1024, output 64x64x512
         
-        self.dec3 = self.upconv(512, 256)
-        self.up3 = self.double_conv(512, 256)
+        self.dec3 = self.upconv(512, 256) # with input 64x64x512, output 128x128x256
+        self.up3 = self.double_conv(512, 256) # with input 128x128x512, output 128x128x256
         
-        self.dec2 = self.upconv(256, 128)
-        self.up2 = self.double_conv(256, 128)
+        self.dec2 = self.upconv(256, 128) # with input 128x128x256, output 256x256x128
+        self.up2 = self.double_conv(256, 128) # with input 256x256x256, output 256x256x128
         
-        self.dec1 = self.upconv(128, 64)
-        self.up1 = self.double_conv(128, 64)
+        self.dec1 = self.upconv(128, 64) # with input 256x256x128, output 512x512x64
+        self.up1 = self.double_conv(128, 64) # with input 512x512x128, output 512x512x64
         
         # Final output
-        self.final = nn.Conv2d(64, out_channels, kernel_size=1)
-        self.sigmoid = nn.Sigmoid() # Sigmoid activation for binary output
+        self.final = nn.Conv2d(64, out_channels, kernel_size=1) # with input 512x512x64, output 512x512x1
+        self.sigmoid = nn.Sigmoid() # Sigmoid activation for 0-1 output # with input 512x512x1, output 512x512x1
     
     def double_conv(self, in_channels, out_channels):
         """Two convolutional layers followed by ReLU."""
